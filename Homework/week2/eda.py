b1320cd6618e51e4dc1f1442038145e44527f763
Tobias <tobiasmaatita@outlook.com>
2018-11-12 15:01:25 +0100

    updated eda with histogram of gdp

--- Homework/week2/eda.py
+++ Homework/week2/eda.py
@@ -5,6 +5,7 @@ import pandas as pd
 import csv
 import statistics as stat
 import matplotlib.pyplot as plt
+import numpy as np
 
 INPUT_FILE = "input.csv"
 WANTED_INFO = ['Country', 'Region', 'Pop. Density (per sq. mi.)',
@@ -42,26 +43,26 @@ def parse(input):
                     missing_keys.append(key)
 
             # add info
-            for info in enumerate(WANTED_INFO):
+            for i, info in enumerate(WANTED_INFO):
 
                 # missing info is 'unknown'
-                if missing_info and info[1] in missing_keys:
-                    countries_info[info[1]].append('unknown')
+                if missing_info and info in missing_keys:
+                    countries_info[info].append(np.nan)
 
                 # convert string to float
-                elif 4 > info[0] > 1:
+                elif 4 > i > 1:
                     # str_to_float = country[info[1]].replace(',', '.')
                     # countries_info[info[1]].append(float(str_to_float))
-                    countries_info[info[1]].append(country[info[1]])
+                    countries_info[info].append(country[info])
 
                 # convert string to int, get rid of 'dollars'
-                elif info[0] == 4:
+                elif i == 4:
                     # countries_info[info[1]].append(int(country[info[1]].strip(' dollars')))
-                    countries_info[info[1]].append(country[info[1]].strip(' dollars'))
+                    countries_info[info].append(country[info].strip(' dollars'))
 
                 # strip blank spaces
                 else:
-                    countries_info[info[1]].append(country[info[1]].strip())
+                    countries_info[info].append(country[info].strip())
 
         # dataframe
         df = pd.DataFrame.from_dict(countries_info)
@@ -74,31 +75,58 @@ def main():
     # obtain dataframe
     df = parse(INPUT_FILE)
 
-    # GDP Data: mean, mode and median
-    # gdps = df['GDP ($ per capita) dollars'].tolist()
+    # extract series GDP
+    gdp_data = df['GDP ($ per capita) dollars']
+
+    # drop NaN and convert to list of ints, whilst cutting outliers
+    gdp_data = gdp_data.dropna(axis=0, how="any")
+    gdp_list = [int(gdp) for gdp in gdp_data.tolist() if int(gdp) <= 100000]
+
+    # inspect max, min for possible outliers in histogram
+    #  turns out there were outliers, data is now cleaned while making gdp_list,
+    #  see justification.txt
+    plt.hist(gdp_list, rwidth = 0.8)
+    plt.xlabel("GDP ($ per capita)")
+    plt.ylabel("Frequency")
+    plt.suptitle("Frequency of GDP worldwide")
+
+    plt.show()
+
+    max_gdp = max(gdp_list)
+    min_gdp = min(gdp_list)
+    print(f"Maximum value: {max_gdp}\nMinimum value: {min_gdp}")
+
+
+
+
+
+
+    # # GDP Data: mean, mode and median
+    # # gdps = df['GDP ($ per capita) dollars'].tolist()
+    # gdps = df['GDP ($ per capita) dollars']
+    # df.query('GDP ($ per capita) dollars != None')
+    # mean = gdps.mean()
+    # print(f"MEAN: {mean}")
     # count = 0
-    # clean_gdps = [int(gdp) for gdp in gdps if not gdp == 'unknown']
-    # gdp_mean = stat.mean(clean_gdps)
+    # clean_gdps = [int(gdp) for gdp in gdps if gdp]
+    # # gdp_mean = stat.mean(clean_gdps)
+    # # gdp_mean = clean_gdps.mean()
     # gdp_mode = stat.mode(clean_gdps)
     # gdp_median = stat.median(clean_gdps)
     # gdp_stdev = stat.stdev(clean_gdps)
     # print(f"mean: {gdp_mean}\nmode: {gdp_mode}\nmedian: {gdp_median}\nstd.dev: {gdp_stdev}")
-
-    # histogram maken?
+    # print(df['GDP ($ per capita) dollars'].tolist())
+    # # histogram maken?\
+    # print(len(clean_gdps))
 
     # infoant mortality
-    infants = df['Infant mortality (per 1000 births)']
-    count = 0
-    for infant in infants:
-        count += 1
-        if infant == 'unknown':
-            print('drop')
-            df.drop(index = 0)
+    # infants = df[df.'Infant mortality (per 1000 births)' != 'unknown']
+    # clean_infants = [float(infant.replace(',','.')) for infant in infants if not infant == 'unknown']
+    # print(len(infants))
 
-    infants = df['Infant mortality (per 1000 births)']
-    # print(infants)
-    print(len(infants))
-    print(len(df))
+    # boxplot = df.boxplot(column=['Infant mortality (per 1000 births)'])
+
+
 
 
 
