ccc035eb29d0a69f184534bc749a2d49ffdc02f5
Tobias <tobiasmaatita@outlook.com>
2018-12-05 19:25:40 +0100

    week 5 scatterplot

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -232,7 +232,7 @@ window.onload = function() {
            .attr('x', leftBuffer + yTextBuffer)
            .attr('y', topBuffer + 100)
            .attr('font-size', '100px')
-           .attr('fill', '#dddd')
+           .attr('fill', '#eee')
 
 
       var colors = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']
